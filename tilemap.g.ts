bbe2b81d86749aad654e089b2489c15d8413a7db
TracyCodeNinja <159206561+TracyCodeNinja@users.noreply.github.com>
2024-02-06 18:16:25 -0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -4,6 +4,8 @@ namespace myTiles {
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -27,7 +29,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100000000000000000000303030000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000001000000000000000000000000000000010000000000000000000000000000050100000000020000010103040301010101010101010101010101010101010101010101010101`, img`
+            case "level1":return tiles.createTilemap(hex`1000100004040404040404040404040404040404000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000010100000000000000000000000100000000000000000000000100000001000000000000000000000001000000010000000000000000010000010000000100000000000000000100000100000001000000000000010501050501030303010505050501010101010101010101010101010101`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -36,15 +38,15 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 2 2 
 . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . 2 . . . . . 
-. . . . . . . . . . 2 . . . . . 
-. . . . . . . . . . 2 . . . . . 
-. . . . . . . . . . 2 . . . . . 
-. . 2 2 . . . 2 2 2 2 2 2 2 2 2 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.collectibleInsignia,sprites.dungeon.hazardLava0,sprites.dungeon.hazardLava1,sprites.builtin.forestTiles0], TileScale.Sixteen);
+. . . . . . . 2 . . . . . . . . 
+. . . . . . . 2 . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . 
+. . . . 2 . . 2 . . . . . . . . 
+. . 2 . 2 . . 2 . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 . 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.collectibleInsignia,sprites.builtin.forestTiles0,myTiles.tile2,sprites.dungeon.hazardLava0], TileScale.Sixteen);
         }
         return null;
     })
@@ -54,6 +56,8 @@ namespace myTiles {
             case "transparency16":return transparency16;
             case "Coble stone":
             case "tile1":return tile1;
+            case "myTile":
+            case "tile2":return tile2;
         }
         return null;
     })
