ea6f8199ac5a7d1180c39eb4ab22f376ecc77e08
TracyCodeNinja <159206561+TracyCodeNinja@users.noreply.github.com>
2024-02-09 18:29:11 -0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,66 @@
+namespace SpriteKind {
+    export const Tool = SpriteKind.create()
+    export const tnt = SpriteKind.create()
+    export const boom = SpriteKind.create()
+    export const Laters = SpriteKind.create()
+    export const na = SpriteKind.create()
+}
+sprites.onDestroyed(SpriteKind.tnt, function (sprite) {
+    scene.cameraShake(50, 200)
+    music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.InBackground)
+    BOOOM = sprites.create(img`
+        . 3 . . . . . . . . . . . 4 . . 
+        . 3 3 . . . . . . . . . 4 4 . . 
+        . 3 d 3 . . 4 4 . . 4 4 d 4 . . 
+        . . 3 5 3 4 5 5 4 4 d d 4 4 . . 
+        . . 3 d 5 d 1 1 d 5 5 d 4 4 . . 
+        . . 4 5 5 1 1 1 1 5 1 1 5 4 . . 
+        . 4 5 5 5 5 1 1 5 1 1 1 d 4 4 . 
+        . 4 d 5 1 1 5 5 5 1 1 1 5 5 4 . 
+        . 4 4 5 1 1 5 5 5 5 5 d 5 5 4 . 
+        . . 4 3 d 5 5 5 d 5 5 d d d 4 . 
+        . 4 5 5 d 5 5 5 d d d 5 5 4 . . 
+        . 4 5 5 d 3 5 d d 3 d 5 5 4 . . 
+        . 4 4 d d 4 d d d 4 3 d d 4 . . 
+        . . 4 5 4 4 4 4 4 4 4 4 4 . . . 
+        . 4 5 4 . . 4 4 4 . . . 4 4 . . 
+        . 4 4 . . . . . . . . . . 4 4 . 
+        `, SpriteKind.boom)
+    BOOOM.changeScale(2, ScaleAnchor.Middle)
+    BOOOM.setPosition(sprite.x, sprite.y)
+    BOOOM.lifespan = 500
+    TriggerBlast()
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    if (HasFlintAndSteel) {
+        tiles.setTileAt(location, assets.tile`myTile0`)
+        music.play(music.createSoundEffect(
+        WaveShape.Noise,
+        499,
+        1,
+        255,
+        255,
+        500,
+        SoundExpressionEffect.Vibrato,
+        InterpolationCurve.Linear
+        ), music.PlaybackMode.InBackground)
+        TNT = sprites.create(assets.image`myImage3`, SpriteKind.tnt)
+        TNT.lifespan = 3000
+        animation.runImageAnimation(
+        TNT,
+        assets.animation`myAnim`,
+        500,
+        true
+        )
+        tiles.placeOnTile(TNT, location)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairNorth, function (sprite, location) {
+    game.setGameOverEffect(true, effects.confetti)
+    game.gameOver(true)
+})
 function startMovie () {
+    Timer = 0
     isMovieOver = false
     scene.setBackgroundColor(7)
     game.splash("Code Ninjas Presents")
@@ -7,11 +69,58 @@ function startMovie () {
     pause(1000)
     isMovieOver = true
 }
+function SpawnTresure () {
+    halo = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . b d b c . . . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . d d 5 1 1 1 1 1 1 1 5 d d . 
+        . . b b 5 5 5 1 1 1 5 5 5 b b . 
+        . . . c c 5 5 5 1 5 5 5 c c . . 
+        . . . . b 5 5 5 1 5 5 5 b . . . 
+        . . . . b b c 5 5 5 c b b . . . 
+        . . . . . . c b d b c . . . . . 
+        . . . . . . . b d b . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.na)
+    halo.changeScale(1.5, ScaleAnchor.Middle)
+    tiles.placeOnTile(halo, tiles.getTileLocation(47, 13))
+    halo.lifespan = 1000
+    treasure = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 8 8 8 8 8 8 8 8 . . . . 
+        . . . 8 9 9 9 9 9 9 9 9 8 . . . 
+        . . 8 8 9 9 9 9 9 9 9 9 8 8 . . 
+        . . 8 8 9 9 9 9 9 9 9 9 8 8 . . 
+        . . . 8 8 8 9 9 9 9 8 8 8 . . . 
+        . . . . . 8 8 9 9 8 8 . . . . . 
+        . . . . . . 8 9 9 8 . . . . . . 
+        . . . . . . . 8 8 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.na)
+    tiles.placeOnTile(treasure, tiles.getTileLocation(47, 13))
+    treasure.follow(MyPlayer, 100)
+}
+function Place_shovle () {
+    shovel = sprites.create(assets.image`Shovle`, SpriteKind.Tool)
+    tiles.placeOnTile(shovel, tiles.getTileLocation(0, 14))
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     MyPlayer.vy = -1000
     MyPlayer.vx = 1000
     MyPlayer.startEffect(effects.trail, 500)
-    music.play(music.melodyPlayable(music.wawawawaa), music.PlaybackMode.InBackground)
+    music.play(music.melodyPlayable(music.jumpUp), music.PlaybackMode.InBackground)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (sprite, location) {
     MyPlayer.vy = -1000
@@ -21,7 +130,7 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (s
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (isMovieOver) {
-        if (MyPlayer.isHittingTile(CollisionDirection.Bottom)) {
+        if (MyPlayer.isHittingTile(CollisionDirection.Bottom) || IsOnLader) {
             music.play(music.melodyPlayable(music.thump), music.PlaybackMode.InBackground)
             MyPlayer.vy += -150
         }
@@ -30,6 +139,28 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
     game.gameOver(false)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Laters, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    MakeLader()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Tool, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    HasFlintAndSteel = true
+    MakeBridge()
+})
+function MakeLader () {
+    tempNum = 14
+    LadderLoop += 14
+    while (LadderLoop > 0) {
+        if (true) {
+            tiles.setTileAt(tiles.getTileLocation(2, LadderLoop), assets.tile`Lader`)
+            music.play(music.melodyPlayable(music.knock), music.PlaybackMode.UntilDone)
+            LadderLoop += -1
+            tempNum += -1
+            pause(500)
+        }
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`CaveMouth`, function (sprite, location) {
     SetLevel2()
 })
@@ -162,20 +293,53 @@ function SetLevel1 () {
 info.onCountdownEnd(function () {
     game.gameOver(true)
 })
+function TriggerBlast () {
+    tiles.setWallAt(tiles.getTileLocation(42, 13), false)
+    tiles.setWallAt(tiles.getTileLocation(42, 14), false)
+    tiles.setTileAt(tiles.getTileLocation(42, 13), assets.tile`myTile0`)
+    tiles.setTileAt(tiles.getTileLocation(42, 14), assets.tile`myTile0`)
+    tiles.setTileAt(tiles.getTileLocation(44, 15), assets.tile`LightBlastfloor0`)
+    tiles.setTileAt(tiles.getTileLocation(43, 15), assets.tile`HevyBlastfloor0`)
+    tiles.setTileAt(tiles.getTileLocation(42, 15), assets.tile`myTile2`)
+    tiles.setTileAt(tiles.getTileLocation(41, 15), assets.tile`HevyBlastfloor`)
+    tiles.setTileAt(tiles.getTileLocation(40, 15), assets.tile`LightBlastfloor`)
+}
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    tiles.setTileAt(location, sprites.dungeon.chestOpen)
+    SpawnTresure()
     music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.UntilDone)
     music.play(music.createSoundEffect(WaveShape.Noise, 1, 571, 0, 255, 500, SoundExpressionEffect.Warble, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
     scene.cameraShake(4, 500)
     music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.UntilDone)
     make_Warden()
+    PlaceLadder()
     info.startCountdown(30)
-    tiles.setTileAt(location, sprites.dungeon.chestOpen)
 })
+function MakeBridge () {
+    Bridgeloop = 9
+    Timer = 1
+    bridgePos = 9
+    while (Bridgeloop > 0) {
+        if (true) {
+            tiles.setWallAt(tiles.getTileLocation(bridgePos, 12), true)
+            tiles.setTileAt(tiles.getTileLocation(bridgePos, 12), assets.tile`Dirt`)
+            music.play(music.melodyPlayable(music.knock), music.PlaybackMode.UntilDone)
+            bridgePos += 1
+            Bridgeloop += -1
+            pause(500)
+        }
+    }
+}
 function SetLevel3 () {
     sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
     scene.setBackgroundImage(assets.image`CityBackDrop`)
-    tiles.placeOnTile(MyPlayer, tiles.getTileLocation(2, 0))
+    tiles.placeOnTile(MyPlayer, tiles.getTileLocation(2, 4))
     tiles.setCurrentTilemap(tilemap`ancient city`)
+    Place_shovle()
+}
+function PlaceLadder () {
+    ladder = sprites.create(assets.image`myLader`, SpriteKind.Laters)
+    tiles.placeOnTile(ladder, tiles.getTileLocation(2, 14))
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
     game.gameOver(false)
@@ -183,9 +347,15 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sp
 function make_Warden () {
     Warden = sprites.create(assets.image`realWarden`, SpriteKind.Enemy)
     tiles.placeOnRandomTile(Warden, sprites.dungeon.collectibleInsignia)
-    Warden.follow(MyPlayer)
-    game.setGameOverEffect(true, effects.starField)
+    Warden.follow(MyPlayer, 100)
+    effects.starField.startScreenEffect()
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.boom, function (sprite, otherSprite) {
+    game.gameOver(false)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Lader`, function (sprite, location) {
+    IsOnLader = true
+})
 function MakePlayer () {
     MyPlayer = sprites.create(assets.image`Steve`, SpriteKind.Player)
     if (game.ask("Press A to play as Alex", "Press B to play as Steve")) {
@@ -323,7 +493,7 @@ function SetLevel2 () {
         `)
     tiles.setCurrentTilemap(tilemap`TheDesent`)
     tiles.placeOnTile(MyPlayer, tiles.getTileLocation(6, 0))
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 1; index++) {
         Rock = sprites.create(assets.image`Rock`, SpriteKind.Enemy)
         Rock.setScale(2, ScaleAnchor.Middle)
         Rock.startEffect(effects.trail)
@@ -338,8 +508,24 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 })
 let Rock: Sprite = null
 let Warden: Sprite = null
+let ladder: Sprite = null
+let bridgePos = 0
+let Bridgeloop = 0
+let LadderLoop = 0
+let tempNum = 0
+let IsOnLader = false
+let shovel: Sprite = null
 let MyPlayer: Sprite = null
+let treasure: Sprite = null
+let halo: Sprite = null
 let isMovieOver = false
+let Timer = 0
+let TNT: Sprite = null
+let HasFlintAndSteel = false
+let BOOOM: Sprite = null
 startMovie()
 MakePlayer()
 SetLevel1()
+game.onUpdateInterval(500, function () {
+    IsOnLader = false
+})
